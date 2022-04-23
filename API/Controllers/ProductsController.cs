9ec24c03a9d4fa3014c5660605b8cf8ebd86b8d9
Nick Kritikos <nikolaos.kritikos@gr.ey.com>
2022-04-23 20:30:13 +0300

    end of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Data;
 using Core.Entities;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -14,23 +15,33 @@ namespace API.Controllers
   
     
     {
+        private readonly IProductRepository _repo;
 
-          private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts(){
-           var products = _context.Products.ToList();
+           var products = await _repo.GetProductsAsync();
 
            return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id){
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands(){
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes(){
+            return Ok(await _repo.GetProductTypesAsync());
         }
     }
 }
