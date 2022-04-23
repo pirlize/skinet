3763edd672b0d727bb3d90dfef9964e7132354d9
Nick Kritikos <nikolaos.kritikos@gr.ey.com>
2022-04-23 11:38:06 +0300

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+
+  
+    
+    {
+
+          private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+           var products = _context.Products.ToList();
+
+           return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
