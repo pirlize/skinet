6ccb60d1c3bce1473f977f64cd97b93dc48cfffc
Nikolaos.Kritikos <nikolaos.kritikos@gr.ey.com>
2023-03-05 16:41:58 +0200

    end of section 8, nav-bar, initial get product

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/Pagination';
+import { Product } from './models/Product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    //httpGet has 14 overloads, in<> you pass return response type
+    this.http
+      .get<Pagination<Product[]>>(
+        'https://localhost:5001/api/products?PageSize=50'
+      )
+      .subscribe({
+        next: (response) => (this.products = response.data),
+        error: (error) => console.log(error),
+        complete: () => {
+          console.log('request completed');
+          console.log('extra statement');
+        },
+      });
+  }
 }
+
+// this.http.get('https://localhost:5001/api/products?PageSize=50').subscribe({
+//   next: (response: any) => (this.products = response.data),
+//   error: (error) => console.log(error),
+//   complete: () => {
+//     console.log('request completed');
+//     console.log('extra statement');
+//   },
+// });
